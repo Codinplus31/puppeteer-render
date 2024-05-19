82c235710b1494e1562ffd0bfe3c0b18117b5ca9
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-05-19 10:16:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,15 @@
 const express = require("express");
-const { scrapeLogic } = require("./scrapeLogic");
+// const { scrapeLogic } = require("./scrapeLogic");
 const app = express();
-const { Octokit } = require('@octokit/rest');
 
-const octokit = new Octokit({
-  auth: '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'
-});
 const PORT = process.env.PORT || 4000;
 
-app.get("/scrape", (req, res) => {
-  scrapeLogic(res);
-});
-
 app.get("/", (req, res) => {
-  res.send("Render Puppeteer server is up and running!");
+  res.send("server is up and running!");
 });
-app.get("/check", (req, res) => {
-  const owner = 'Codinplus31';
-const repo = 'puppeteer-render';
-const branch = 'main';
-const path = 'check.txt';
-const message = 'Update check.txt';
-const content = Buffer.from('New content').toString('base64');
-const sha = ''; // Set this if you want to update an existing file
 
-octokit.repos.createOrUpdateFileContents({
-  owner,
-  repo,
-  path,
-  branch,
-  message,
-  content,
-  sha
-}).then(response => {
-  console.log('File updated successfully:', response.data);
-   res.send('File updated successfully:'+ response.data);
-}).catch(error => {
-  console.error('Error updating file:', error);
-});
-  res.send("Render Puppeteer server is up and running!");
+app.get("/check", (req, res) => {
+ 
 });
 
 app.listen(PORT, () => {
